4d4cac29be084a8f1843172079c39d8f2841572a
Apjuve <abiponce3@hotmail.com>
2022-10-24 22:51:45 -0400

    created navbar

---
+++ prestigebr/src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+
+function Navbar ({currentPage, handlePageChange}) {
+    return (
+        <nav>
+            <div>
+                <a href="/">
+                    PrestigeBarber 
+                </a>
+            </div>
+
+            <div>
+                <ul>
+                <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange("About")}
+                className={
+                  currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#Services"
+                onClick={() => handlePageChange("Services")}
+                className={
+                  currentPage === "Services" ? "nav-link active" : "nav-link"
+                }
+              >
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange("Contact")}
+                className={
+                  currentPage === "Contact" ? "nav-link active" : "nav-link"
+                }
+              >
+                Contact
+              </a>
+            </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
