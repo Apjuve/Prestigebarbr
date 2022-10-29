dc3419991d70c5b765cd29c39315d8189a30eb21
Apjuve <abiponce3@hotmail.com>
2022-10-29 18:38:42 -0400

    added sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import Hero from './components/sections/Hero.js'
+import About from "./components/sections/About.js"
+import Contact from "./components/sections/Contact.js"
+import Footer from "./components/sections/Footer.js"
+import Header from "./components/sections/Header.js"
+import Home from "./components/sections/Home.js"
+import Services from "./components/sections/Services.js"
+
+
+
+
 
 function App() {
     return (
-        <>Hello World</>
+        <>
+        <Hero/>
+        <Header/>
+        <Home/>
+        <About/>
+        <Services/>
+        <Footer/>
+        
+
+        </>
     );
 }
 
