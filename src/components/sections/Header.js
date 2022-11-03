5b1fc231d822c76b9442d06482e9882a3e0b3d89
temptado <jason1287712@gmail.com>
2022-11-02 20:44:31 -0400

    added script to change header background on scroll

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const header = () => {
+  function scrollHeader() {
+    const header = document.getElementById('header');
+
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+  }
+
+  window.addEventListener('scroll', scrollHeader);
   return (
     <header className="header" id="header">
       <nav className="nav container">
