14990aa6e4ea33da24ebb8acfc2437399400a80f
temptado <jason1287712@gmail.com>
2022-11-17 21:55:29 -0500

    added new format images with new images added to style section

--- src/components/More.js
+++ src/components/More.js
@@ -1,6 +1,13 @@
 // import Styles from './sections/Styles';
 // import Barbers from './sections/Barbers';
-import lineup from '../images/Lineup.jpg';
+// import lineup from '../images/Lineup.jpg';
+import style1 from '../images/Barber1.jpg';
+import style2 from '../images/Barber2.jpg';
+import style3 from '../images/barber3.jpg';
+import style4 from '../images/barber4.jpg';
+import style5 from '../images/barber5.jpg';
+import style6 from '../images/barber6.jpg';
+
 
 import React from 'react'
 
@@ -11,45 +18,26 @@ const More = () => {
 
             <div className="style__images">
                 <div className="img__block">
-                    <img src={lineup} alt="" />
-                    <div className="text__box">
-                        <p>Line-ups</p>
-                    </div>
+                    <img src={style1} alt="" />
                 </div>
                 <div className="img__block">
-                    <img src={lineup} alt="" />
-                    <div className="text__box">
-                        <p>Beard Trims</p>
-                    </div>
+                    <img src={style2} alt="" />
                 </div>
                 <div className="img__block">
-                    <img src={lineup} alt="" />
-                    <div className="text__box">
-                        <p>Styling</p>
-                    </div>
-                </div>
+                    <img src={style3} alt="" />
 
-
+                </div>
                 <div className="img__block">
-                    <img src={lineup} alt="" />
-                    <div className="text__box">
-                        <p>Quality Products</p>
-                    </div>
+                    <img src={style4} alt="" />
                 </div>
                 <div className="img__block">
                     <img
-                        src={lineup}
+                        src={style5}
                         alt=""
                     />
-                    <div className="text__box">
-                        <p>Finest Equipment</p>
-                    </div>
                 </div>
                 <div className="img__block">
-                    <img src={lineup} alt="" />
-                    <div className="text__box">
-                        <p>Comfortable Experience</p>
-                    </div>
+                    <img src={style6} alt="" />
                 </div>
 
             </div>
