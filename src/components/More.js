6a131e17df76bb113385c40bcbb5f04d6fb2d1a4
temptado <jason1287712@gmail.com>
2022-11-18 20:14:50 -0500

    added new images to more section

--- src/components/More.js
+++ src/components/More.js
@@ -1,12 +1,30 @@
 // import Styles from './sections/Styles';
 // import Barbers from './sections/Barbers';
-// import lineup from '../images/Lineup.jpg';
 import style1 from '../images/Barber1.jpg';
 import style2 from '../images/Barber2.jpg';
 import style3 from '../images/barber3.jpg';
 import style4 from '../images/barber4.jpg';
 import style5 from '../images/barber5.jpg';
 import style6 from '../images/barber6.jpg';
+import style7 from '../images/barber7.jpg';
+import style8 from '../images/barber8.jpg';
+import style9 from '../images/barber9.jpg';
+import style10 from '../images/barber10.jpg';
+import style11 from '../images/barber11.jpg';
+import style12 from '../images/barber12.jpg';
+import style13 from '../images/barber13.jpg';
+import style14 from '../images/barber14.jpg';
+import style15 from '../images/barber15.jpg';
+import style16 from '../images/barber16.jpg';
+import style17 from '../images/barber17.jpg';
+import style18 from '../images/barber18.jpg';
+import style19 from '../images/barber19.jpg';
+import style20 from '../images/barber20.jpg';
+import style21 from '../images/barber21.jpg';
+import style22 from '../images/barber22.jpg';
+import style23 from '../images/barber23.jpg';
+import style24 from '../images/barber24.jpg';
+
 
 
 import React from 'react'
@@ -39,7 +57,60 @@ const More = () => {
                 <div className="img__block">
                     <img src={style6} alt="" />
                 </div>
-
+                <div className="img__block">
+                    <img src={style7} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style8} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style9} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style10} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style11} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style12} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style13} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style14} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style15} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style16} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style17} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style18} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style19} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style20} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style21} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style22} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style23} alt="" />
+                </div>
+                <div className="img__block">
+                    <img src={style24} alt="" />
+                </div>
             </div>
         </section>
     )
