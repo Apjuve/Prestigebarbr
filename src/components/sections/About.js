2703a010c0a9d87c4a9f8743edacfad04a14709d
Apjuve <abiponce3@hotmail.com>
2023-03-11 12:51:39 -0500

    updated about me page

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -10,10 +10,19 @@ const about = () => {
         <div className="about__text">
           <h3>Prestige Barbershop</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
-            commodi, alias, fuga nemo maiores ex minus consectetur, provident
-            corrupti odio perferendis? Neque culpa dolorum omnis excepturi vitae
-            debitis asperiores cum!
+          Welcome to our Prestige barber website! We are thrilled to have you here and would like to take a moment to introduce ourselves.
+          
+
+          Our barbershop is dedicated to providing you with the ultimate grooming experience. Our team of skilled barbers are passionate about their craft and are committed to delivering the best haircuts, shaves, fades and grooming services possible.
+          
+
+          We believe that a great haircut is about more than just the end result - it's about the experience. That's why we've created a warm and inviting atmosphere that will make you feel right at home. Whether you're looking for a classic or modern haircut, our barbers will work with you to achieve the look you want.
+          
+
+          We offer a range of services including haircuts, fades, beard trims, and more. Our shop is equipped with the latest tools and technology to ensure that you receive the best possible results.
+         
+          So whether you're in need of a quick trim or a full grooming session, we've got you covered. We look forward to seeing you at our Prestige barbershop soon!
+
           </p>
         </div>
         <div className="about__images">
